29821aceb81627628ccd8798177100911355298b
femi <femilawal76@gmail.com>
2019-10-01 19:59:25 +0100

    set up

---
+++ api.js
@@ -0,0 +1,9 @@
+AIzaSyCUJntBLUcjjeNX - m2RV1xsdbFc2OBXCh8 - API KEY
+
+
+004342468873599729435: yikok5bxamu - search engine id
+
+
+Twillo account sid: AC171c16f67e0d9e445a5d04c2f245ae2a
+
+Twillo Auth Token: 62 c7230d40b2b2cd1a60a3e4ae7a5a15
